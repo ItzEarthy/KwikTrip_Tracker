0fda2e9f7d4ef66fd333fd00f5e13ad51a6705d0
ItzEarthy <68570771+ItzEarthy@users.noreply.github.com>
2025-05-31 19:33:41 -0500

    Update FriendDashboard.jsx

--- frontend/src/components/FriendDashboard.jsx
+++ frontend/src/components/FriendDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import Layout from "./Layout";
 import { useNavigate } from "react-router-dom";
-const API_BASE = import.meta.env.VITE_API_URL || "http://localhost:3001/api/";
+const API_BASE = import.meta.env.VITE_API_URL || "http://192.168.86.33:3001/api/";
 
 
 export default function FriendDashboard() {
